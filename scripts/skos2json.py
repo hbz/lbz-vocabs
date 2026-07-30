04072757667fc67dc6349a1ce11cc381a922b7e7
Phu Tu <phu.tu@hbz-nrw.de>
2026-07-30 13:39:06 +0200

    Add optional argument --no-top-levels to skos2json.py
    - includes skos:broader in the SPARQL query
    - uses this argument for rpb.ttl + rpb-spatial.ttl only
    
    see https://github.com/hbz/lbz-vocabs/issues/13

--- scripts/skos2json.py
+++ scripts/skos2json.py
@@ -1,28 +1,45 @@
 import rdflib
 import sys
 import os
+import argparse
 
 if not os.path.exists('output'):
    os.makedirs('output')
 
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", help="Path to the input Turtle file")
+parser.add_argument(
+    "--no-top-levels",
+    action="store_true",
+    help="Include skos:broader in the SPARQL query",
+)
+args = parser.parse_args()
+
+input_file = args.input_file
+no_top_levels = args.no_top_levels
+
 g = rdflib.Graph()
 
 # Load file in format ttl
-input_file = sys.argv[1]
 g.parse(input_file, format="ttl")
 
 # SPARQL query for URI and label of each concept
-qres = g.query(
-    """
+predicate_pattern = "skos:prefLabel ?label ."
+if no_top_levels:
+    predicate_pattern = "skos:prefLabel ?label ;\n                skos:broader ?broader ."
+
+query = f"""
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
     SELECT ?label ?concept ?definition
-       WHERE {
+       WHERE {{
            ?concept a skos:Concept ;
-                skos:prefLabel ?label ;
-                skos:broader ?broader .
-               OPTIONAL {?concept skos:definition ?definition}
-       }""")
+                {predicate_pattern}
+           OPTIONAL {{?concept skos:definition ?definition}}
+       }}
+"""
+
+qres = g.query(query)
 
 # Write results to file per line
 with open("output/%s.ndjson" % input_file.replace(".ttl",""), "w") as output:
