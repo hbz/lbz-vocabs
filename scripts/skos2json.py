8ec3e3bc451693f8c5cfe6ed74d99dae5f5ea298
Phu Tu <phu.tu@hbz-nrw.de>
2026-09-15 15:13:25 +0200

    Fix invalid f-string
    see https://github.com/hbz/lbz-vocabs/issues/30

--- scripts/skos2json.py
+++ scripts/skos2json.py
@@ -24,7 +24,7 @@ g = rdflib.Graph()
 g.parse(input_file, format="ttl")
 
 # SPARQL query for URI and label of each concept
-predicate_pattern = f"skos:prefLabel ?label {"; skos:broader ?broader" if no_top_levels else ""} ."
+predicate_pattern = f'skos:prefLabel ?label {"; skos:broader ?broader" if no_top_levels else ""} .'
 
 query = f"""
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
